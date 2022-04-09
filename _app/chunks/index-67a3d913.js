e8dadfa8137c1536c37316d0b207ffc8318d36f3
geofranzi <geofranzi@users.noreply.github.com>
2022-04-09 23:04:08 +0000

    deploy: 56cd2c92a3944044bdc0fba70d7ceb3e8ca7cb37

---
+++ _app/chunks/index-67a3d913.js
@@ -0,0 +1,1 @@
+import{a2 as n}from"./index-eb0a202e.js";function r(t,{delay:o=0,duration:e=400,easing:a=n}={}){const i=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:a,css:c=>`opacity: ${c*i}`}}export{r as f};
