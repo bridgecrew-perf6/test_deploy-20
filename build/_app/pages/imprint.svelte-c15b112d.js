6f4b0bba9f69bec271c8b37e5eb7ce298651bde8
Franzi <franzi82@gmx.de>
2022-04-10 00:30:24 +0200

    Add build

---
+++ build/_app/pages/imprint.svelte-c15b112d.js
@@ -0,0 +1,53 @@
+import{S as ze,i as Pe,s as ke,k as u,e as r,t as a,$ as He,d as s,m as d,c as i,a as o,h as l,b as we,g as me,_ as e,n as ue}from"../chunks/index-3e01cf4c.js";import{b as Be}from"../chunks/env-859c48a7.js";function _e(De){let v,t,p,E,A,I,S,m,W,R,h,K,x,V,F,G,L,M,N,j,T,D,y,C,g,q,J,Z,O,z,U,$,b,Q,X,Y,ee,P,te,ne,k,re,ie,H,ae,le,w,se;return{c(){v=u(),t=r("div"),p=r("h1"),E=a("Impressum"),A=u(),I=r("br"),S=u(),m=r("h2"),W=a("Adresse:"),R=u(),h=r("p"),K=a(`Deutsches Zentrum f\xFCr integrative Biodiversit\xE4tsforschung (iDiv) Halle-Jena-Leipzig, Puschstra\xDFe
+		4, 04103 Leipzig`),x=r("br"),V=a(`
+		E-Mail: info@planthub.de`),F=r("br"),G=a(`
+		Tel.: +49 341 9733105`),L=r("br"),M=a(`
+		Fax: +49 341 9739350`),N=r("br"),j=a(`
+		Web: www.planthub.de`),T=u(),D=r("h2"),y=a("Herausgeber:"),C=u(),g=r("p"),q=a(`PlantHub, eine Support Unit des Deutschen Zentrums f\xFCr integrative Biodiversit\xE4tsforschung
+		(iDiv) Halle-Jena-Leipzig`),J=r("br"),Z=a(`
+		Das Forschungszentrum iDiv wird vertreten durch das Speaker Board (verantwortlich im Sinne des Presserechts):
+		Prof. Dr. Tiffany Knight, Prof. Dr. Nicole van Dam, Prof. Dr. Henrique M. Pereira, Prof. Dr. Nico
+		Eisenhauer (stellvertrendend f\xFCr Prof. Dr. Christian Wirth)`),O=u(),z=r("h2"),U=a("Inhaltlich Verantwortliche:"),$=u(),b=r("p"),Q=a(`Redaktion: Dr. David Schellenberger Costa (PlantHub-Website), Dr. Samira Babolou, (iKNOW,
+		Knowledge Graphs)`),X=r("br"),Y=a(`
+		Kontakt: planthub@iDiv.de`),ee=u(),P=r("h2"),te=a("Haftungsausschluss:"),ne=u(),k=r("p"),re=a(`Die ver\xF6ffentlichten Informationen sind sorgf\xE4ltig zusammengestellt, erheben aber keinen
+		Anspruch auf Vollst\xE4ndigkeit oder Aktualit\xE4t; eine entsprechende Gew\xE4hr wird nicht \xFCbernommen.
+		PlantHub haftet nicht f\xFCr die Richtigkeit der Inhalte, Fehler redaktioneller und technischer
+		Art, Auslassungen oder \xC4hnliches auf seinen Webseiten. Die Nutzung der abrufbaren Inhalte
+		erfolgt auf eigene Gefahr. Alle kostenfreien Angebote sind unverbindlich. PlantHub beh\xE4lt es
+		sich vor, jederzeit ohne vorherige Ank\xFCndigung das Angebot zu ver\xE4ndern, zu erg\xE4nzen, zu l\xF6schen
+		oder die Ver\xF6ffentlichung einzustellen. Wir m\xF6chten Sie bitten, Fehler inhaltlicher oder
+		technischer Art an unsere daf\xFCr eingerichtete Mail mitzuteilen: info@planthub.de Trotz
+		sorgf\xE4ltiger inhaltlicher Kontrolle \xFCbernimmt PlantHub keine Haftung f\xFCr die Inhalte von Links
+		zu externen Webseiten Dritter. Diese Webseiten und ihre Inhalte unterliegen der Haftung des
+		jeweiligen Betreibers. Die Redaktion hat keinen Einfluss auf die aktuelle und zuk\xFCnftige
+		Gestaltung und Inhalte der verlinkten Seiten. PlantHub macht sich den Inhalt der verkn\xFCpften
+		Seiten nicht zu eigen.`),ie=u(),H=r("h2"),ae=a("Verwendung von Bildern:"),le=u(),w=r("p"),se=a(`Die Rechte an den Bildern auf den Webseiten von PlantHub liegen, sofern nicht anders angegeben,
+		bei PlantHub. Die Bilder d\xFCrfen nicht verwendet werden. Eine m\xF6gliche \xDCbertragung von
+		Bildnutzungsrechten kann, nach jeweiliger Pr\xFCfung, auf Anfrage erfolgen. Wir m\xF6chten Sie bitten,
+		f\xFCr Bildanfragen die daf\xFCr eingerichtete E-Mail zu nutzen: info@planthub.de`),this.h()},l(f){He('[data-svelte="svelte-1v8s8bm"]',document.head).forEach(s),v=d(f),t=i(f,"DIV",{class:!0});var n=o(t);p=i(n,"H1",{});var de=o(p);E=l(de,"Impressum"),de.forEach(s),A=d(n),I=i(n,"BR",{}),S=d(n),m=i(n,"H2",{});var oe=o(m);W=l(oe,"Adresse:"),oe.forEach(s),R=d(n),h=i(n,"P",{});var c=o(h);K=l(c,`Deutsches Zentrum f\xFCr integrative Biodiversit\xE4tsforschung (iDiv) Halle-Jena-Leipzig, Puschstra\xDFe
+		4, 04103 Leipzig`),x=i(c,"BR",{}),V=l(c,`
+		E-Mail: info@planthub.de`),F=i(c,"BR",{}),G=l(c,`
+		Tel.: +49 341 9733105`),L=i(c,"BR",{}),M=l(c,`
+		Fax: +49 341 9739350`),N=i(c,"BR",{}),j=l(c,`
+		Web: www.planthub.de`),c.forEach(s),T=d(n),D=i(n,"H2",{});var ce=o(D);y=l(ce,"Herausgeber:"),ce.forEach(s),C=d(n),g=i(n,"P",{});var B=o(g);q=l(B,`PlantHub, eine Support Unit des Deutschen Zentrums f\xFCr integrative Biodiversit\xE4tsforschung
+		(iDiv) Halle-Jena-Leipzig`),J=i(B,"BR",{}),Z=l(B,`
+		Das Forschungszentrum iDiv wird vertreten durch das Speaker Board (verantwortlich im Sinne des Presserechts):
+		Prof. Dr. Tiffany Knight, Prof. Dr. Nicole van Dam, Prof. Dr. Henrique M. Pereira, Prof. Dr. Nico
+		Eisenhauer (stellvertrendend f\xFCr Prof. Dr. Christian Wirth)`),B.forEach(s),O=d(n),z=i(n,"H2",{});var fe=o(z);U=l(fe,"Inhaltlich Verantwortliche:"),fe.forEach(s),$=d(n),b=i(n,"P",{});var _=o(b);Q=l(_,`Redaktion: Dr. David Schellenberger Costa (PlantHub-Website), Dr. Samira Babolou, (iKNOW,
+		Knowledge Graphs)`),X=i(_,"BR",{}),Y=l(_,`
+		Kontakt: planthub@iDiv.de`),_.forEach(s),ee=d(n),P=i(n,"H2",{});var ge=o(P);te=l(ge,"Haftungsausschluss:"),ge.forEach(s),ne=d(n),k=i(n,"P",{});var be=o(k);re=l(be,`Die ver\xF6ffentlichten Informationen sind sorgf\xE4ltig zusammengestellt, erheben aber keinen
+		Anspruch auf Vollst\xE4ndigkeit oder Aktualit\xE4t; eine entsprechende Gew\xE4hr wird nicht \xFCbernommen.
+		PlantHub haftet nicht f\xFCr die Richtigkeit der Inhalte, Fehler redaktioneller und technischer
+		Art, Auslassungen oder \xC4hnliches auf seinen Webseiten. Die Nutzung der abrufbaren Inhalte
+		erfolgt auf eigene Gefahr. Alle kostenfreien Angebote sind unverbindlich. PlantHub beh\xE4lt es
+		sich vor, jederzeit ohne vorherige Ank\xFCndigung das Angebot zu ver\xE4ndern, zu erg\xE4nzen, zu l\xF6schen
+		oder die Ver\xF6ffentlichung einzustellen. Wir m\xF6chten Sie bitten, Fehler inhaltlicher oder
+		technischer Art an unsere daf\xFCr eingerichtete Mail mitzuteilen: info@planthub.de Trotz
+		sorgf\xE4ltiger inhaltlicher Kontrolle \xFCbernimmt PlantHub keine Haftung f\xFCr die Inhalte von Links
+		zu externen Webseiten Dritter. Diese Webseiten und ihre Inhalte unterliegen der Haftung des
+		jeweiligen Betreibers. Die Redaktion hat keinen Einfluss auf die aktuelle und zuk\xFCnftige
+		Gestaltung und Inhalte der verlinkten Seiten. PlantHub macht sich den Inhalt der verkn\xFCpften
+		Seiten nicht zu eigen.`),be.forEach(s),ie=d(n),H=i(n,"H2",{});var ve=o(H);ae=l(ve,"Verwendung von Bildern:"),ve.forEach(s),le=d(n),w=i(n,"P",{});var pe=o(w);se=l(pe,`Die Rechte an den Bildern auf den Webseiten von PlantHub liegen, sofern nicht anders angegeben,
+		bei PlantHub. Die Bilder d\xFCrfen nicht verwendet werden. Eine m\xF6gliche \xDCbertragung von
+		Bildnutzungsrechten kann, nach jeweiliger Pr\xFCfung, auf Anfrage erfolgen. Wir m\xF6chten Sie bitten,
+		f\xFCr Bildanfragen die daf\xFCr eingerichtete E-Mail zu nutzen: info@planthub.de`),pe.forEach(s),n.forEach(s),this.h()},h(){document.title="PlantHub - Imprint",we(t,"class","content")},m(f,he){me(f,v,he),me(f,t,he),e(t,p),e(p,E),e(t,A),e(t,I),e(t,S),e(t,m),e(m,W),e(t,R),e(t,h),e(h,K),e(h,x),e(h,V),e(h,F),e(h,G),e(h,L),e(h,M),e(h,N),e(h,j),e(t,T),e(t,D),e(D,y),e(t,C),e(t,g),e(g,q),e(g,J),e(g,Z),e(t,O),e(t,z),e(z,U),e(t,$),e(t,b),e(b,Q),e(b,X),e(b,Y),e(t,ee),e(t,P),e(P,te),e(t,ne),e(t,k),e(k,re),e(t,ie),e(t,H),e(H,ae),e(t,le),e(t,w),e(w,se)},p:ue,i:ue,o:ue,d(f){f&&s(v),f&&s(t)}}}const Ie=Be;class Se extends ze{constructor(v){super(),Pe(this,v,null,_e,ke,{})}}export{Se as default,Ie as router};
